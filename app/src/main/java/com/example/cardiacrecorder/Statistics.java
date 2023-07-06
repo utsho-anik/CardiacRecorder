368a169438ccbc4cbe393e50e8cd9ae32c9532d3
n-error <nibirjakaria@gmail.com>
2023-07-06 12:21:52 +0600

    n commit

--- app/src/main/java/com/example/cardiacrecorder/Statistics.java
+++ app/src/main/java/com/example/cardiacrecorder/Statistics.java
@@ -1,5 +1,6 @@
 package com.example.cardiacrecorder;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -38,6 +39,7 @@ public class Statistics extends AppCompatActivity {
     TextInputLayout textInputLayout;
     AutoCompleteTextView autoCompleteTextView;
 
+    @SuppressLint("SuspiciousIndentation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
